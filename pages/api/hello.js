6a44bd31514ca6b7ab0772f5f33b9f2e757a4374
mouhsineelachbi <mouhsineelachbi@gmail.com>
2020-11-04 22:31:24 +0100

    fix

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default (req, res) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
-}
+  res.statusCode = 200;
+  res.json({ name: "John Doe" });
+};
+
+/*
+Credit to https://www.smashingmagazine.com/2020/10/graphql-server-next-javascript-api-routes/
+ */
